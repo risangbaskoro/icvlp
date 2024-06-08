ba1b6f5776e07255f737d9a696782313cad72b61
Risang Baskoro <contact@risangbaskoro.com>
2024-06-08 07:57:26 +0700

    Download YouTube video script

---
+++ download_videos.py
@@ -0,0 +1,58 @@
+import json
+import os
+import random
+import sys
+import time
+
+import logging
+
+logging.basicConfig(filename=f"logs/download_{time.strftime('%Y%m%d')}.log", level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+YOUTUBE_DOWNLOADER = "yt-dlp"
+
+
+def check_youtube_dl_version():
+    version = os.popen(f'{YOUTUBE_DOWNLOADER} --version').read()
+    assert version, f"{YOUTUBE_DOWNLOADER} cannot be found in PATH. Please verify your installation."
+
+
+def download_youtube_videos(index_file, saveto='raw_videos'):
+    content = json.load(open(index_file))
+
+    if not os.path.exists(saveto):
+        os.mkdir(saveto)
+
+    for entry in content:
+        label = entry['label']
+        video_id = entry['video_id']
+        video_url = entry['url']
+
+        if 'youtube' not in video_url and 'youtu.be' not in video_url:
+            continue
+
+        if (os.path.exists(os.path.join(saveto, video_id + 'mp4'))
+                or os.path.exists(os.path.join(saveto, video_id + 'mkv'))):
+            logging.info(f'YouTube video {video_id} is already exists.')
+            continue
+        else:
+            cmd = f"{YOUTUBE_DOWNLOADER} \"{video_url}\" -o \"{os.path.join(saveto, video_id + '.%(ext)s')}\" -f mp4"
+
+            rv = os.system(cmd)
+
+            if not rv:
+                logging.info(f'Finish downloading YouTube video url {video_url}')
+            else:
+                logging.error(f'Unsuccessful downloading YouTube video url {video_url}')
+
+            # Reduce the download frequency, avoid spam
+            time.sleep(random.uniform(1.0, 1.5))
+
+
+if __name__ == '__main__':
+    index = 'icvlp_v0.1.json'
+
+    logging.info('Start downloading YouTube videos.')
+    logging.info(f'Index file: {index}')
+    check_youtube_dl_version()
+    download_youtube_videos(index)
