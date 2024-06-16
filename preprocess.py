5c3368df765bdbd581b83a3d7b72b0b87761a865
Risang Baskoro <contact@risangbaskoro.com>
2024-06-16 20:13:27 +0700

    Add preprocess.py and add_frames.py

---
+++ preprocess.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import glob
+import json
+import os
+import sys
+import time
+
+import cv2
+
+from tqdm import tqdm, trange
+
+import logging
+
+logging.basicConfig(filename=f"logs/extract_frames_{time.strftime('%Y%m%d')}.log", level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+
+def extract_frame(video_path, output_dir, plate, instance_num):
+    cap = cv2.VideoCapture(video_path)
+    label = plate['label']
+
+    plates_extracted = 0
+    for frame in plate['frames']:
+        output_name = f"{label}_{instance_num + 1}.jpeg"
+        output_path = os.path.join(output_dir, output_name)
+        frame_number = frame['frame']
+        bbox = frame['bbox']
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+        ret, image = cap.read()
+
+        # x_min, y_min, xmax, y_max
+        img = image[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+        cv2.imwrite(output_path, img)
+        plates_extracted += 1
+        instance_num += 1
+
+    return plates_extracted
+
+
+def extract_frames(index_file, output_dir):
+    content = json.load(open(index_file))
+
+    for video in content:
+        filename = video['video_id'] + '.mp4'
+        video_path = os.path.join('raw_videos', filename)
+
+        if not os.path.exists(video_path):
+            logging.info(f'Skipping {video_path}: video does not exist')
+            continue
+
+        plates = video['plates']
+        for plate in plates:
+            if not len(plate['frames']):
+                continue
+            existing_plate_files = [int(num.split('_')[-1].split('.')[0]) for num in
+                                    glob.glob(os.path.join(output_dir, plate['label'] + '*.jpeg'))]
+            instance_num = sorted(existing_plate_files, reverse=True)[0] if existing_plate_files else 0
+            extract_frame(video_path, output_dir, plate, instance_num)
+
+
+if __name__ == '__main__':
+    INDEX_FILE = 'icvlp_v0.1.json'
+    TARGET_DIR = 'images'
+
+    if not os.path.exists(TARGET_DIR):
+        os.mkdir(TARGET_DIR)
+
+    extract_frames(INDEX_FILE, TARGET_DIR)
---
+++ utils/add_frames.py
@@ -0,0 +1,64 @@
+import glob
+import json
+import os
+
+import xml.etree.ElementTree as et
+
+
+def get_bounding_boxes(xml_path):
+    if not os.path.exists(xml_path):
+        return
+    tree = et.parse(xml_path)
+    root = tree.getroot()
+
+    filename = root.find('filename').text.split('.')[0]
+
+    video_id, frame, label = filename.split('_')
+
+    obj = root.find('object')
+
+    if obj is None:
+        return
+
+    bbox = obj.find('bndbox')
+    x_min = round(float(bbox.find('xmin').text))
+    y_min = round(float(bbox.find('ymin').text))
+    x_max = round(float(bbox.find('xmax').text))
+    y_max = round(float(bbox.find('ymax').text))
+    bbox = [x_min, y_min, x_max, y_max]
+
+    return {
+        'frame': int(frame),
+        'bbox': bbox
+    }
+
+
+def get_xml_files(plate_data):
+    label = plate_data['label']
+    pattern = os.path.join('frames', f'*_{label}.*')
+    filenames = glob.glob(pattern)
+    tmp = []
+    for filename in filenames:
+        video_id, frame, label = os.path.basename(filename).split('.')[0].split('_')
+        tmp.append((video_id, int(frame), label))
+    tmp = sorted(tmp)
+    filenames = ['_'.join([video_id, str(frame), label + '.xml']) for video_id, frame, label in tmp]
+    return filenames
+
+
+if __name__ == '__main__':
+    INDEX_FILE = os.path.join(os.getcwd(), 'icvlp_v0.1.json')
+    content = json.load(open(INDEX_FILE))
+
+    for video in content:
+        for plate in video['plates']:
+            paths = get_xml_files(plate)
+            plate['frames'] = []
+            for path in paths:
+                path = os.path.join('annotations', path)
+                box = get_bounding_boxes(path)
+                if box is None:
+                    continue
+                plate['frames'].append(box)
+
+    json.dump(content, open(INDEX_FILE, 'w'), indent=2)
